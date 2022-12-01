4243d779238d653f2a39da871004216b30b39635
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2022-12-02 01:21:23 +0200

    task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const totalCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${totalCategories.length}`);
+
+const categoriesArray = [...totalCategories]
+  .map(
+    (element) => `Categoriy: ${element.children[0].textContent}
+Elements: ${element.children[1].children.length}`
+  )
+  .join("\n");
+console.log(categoriesArray);
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("ul#ingredients");
+
+const getLiMarkup = (ingredient) => `<li class="item">${ingredient}</li>`;
+const getListMarkup = (ingredientsList) =>
+  ingredientsList.map((it) => getLiMarkup(it)).join("");
+
+ingredientsRef.insertAdjacentHTML("afterbegin", getListMarkup(ingredients));
