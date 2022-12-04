9c56fab0e34687a3db2231eca92a4bd64a874e5a
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2022-12-04 20:20:39 +0200

    task-6

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", (event) => {
+  if (event.target.value.length === Number(input.dataset.length)) {
+    input.classList.add("valid");
+    if (input.classList.contains("invalid")) {
+      input.classList.remove("invalid");
+    }
+    return;
+  }
+  input.classList.add("invalid");
+  if (input.classList.contains("valid")) {
+    input.classList.remove("valid");
+  }
+});
