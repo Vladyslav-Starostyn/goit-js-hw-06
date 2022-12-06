e0671c5647b07b3e0c8e9bfa5fda6d7ddfc732e7
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2022-12-06 19:03:11 +0200

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log(alert("Please fill in all the fields!"));
+  }
+
+  console.log({ email: email.value, password: password.value });
+  event.currentTarget.reset();
+}
