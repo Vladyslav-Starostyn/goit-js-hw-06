807aef47f6592dd13220bbfcb894186084d88a34
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-01-12 18:15:10 +0200

    tassk-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+const arrayIngredients = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  arrayIngredients.push(item);
+});
+
+listEl.append(...arrayIngredients);
+
+console.log(listEl);
